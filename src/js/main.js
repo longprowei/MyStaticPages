b021f83463d61eca0ce273e57691eaac3e70cb43
Chenglong Wei <longprowei@gmail.com>
2018-05-07 00:07:20 +1000

    added background and changed a some style and text

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
-'use strict';
-
-console.log('main.js test');
+/*!
+ * main.js
+ * All JavaScript for Chenglong Wei's Home website
+ * Author: Chenglong Wei
+ * github: https://github.com/longprowei
+ */
+(function() {
+  'use strict';
+  console.log('Are you interested my site? Please access my Github: \n');
+  console.log('https://github.com/longprowei \n');
+  console.log('for source code.');
+})();
